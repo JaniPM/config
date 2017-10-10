8c3e2dba1ff478bf91e6359c72d9af0f8daf6dbc
Jani Siivola <jani.siivola@moraius.com>
2017-10-10 16:45:58 +0800

    Added Atom pakcage installations

---
+++ atomPackageInstalls.sh
@@ -0,0 +1,14 @@
+
+# !/bin/sh
+
+# Remember to make this executalbe and readable script
+# chmod a+rx atomPackageInstalls.sh
+
+echo Installing Atom packages
+apm install linter-eslint
+apm install nuclide
+apm install linter-ui-default
+apm install docblockr
+apm install language-babel
+apm install platformio-ide-terminal
+apm install vim-mode-plus
